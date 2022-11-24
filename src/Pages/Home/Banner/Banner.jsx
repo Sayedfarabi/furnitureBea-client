1e09385ae3152ff601fab7cca6d7abfa9e76abf7
Sayedfarabi <sayedfarabi6099@gmail.com>
2022-11-24 10:00:56 +0600

    banner done

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -7,10 +7,11 @@ const Banner = () => {
         <div>
             <div className="hero h-96" style={{ backgroundImage: `url(${bannerImg})` }}>
                 <div className="hero-overlay bg-opacity-60"></div>
-                <div className="hero-content text-center text-neutral-content">
+                <div className="hero-content text-center text-base-100">
                     <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                        <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <h1 className="mb-5 text-5xl font-bold">Hello Users</h1>
+                        <p className="mb-5">This is our used furniture buying or selling site. Buying used furniture can be awesome or not so awesome.
+                            So, select your Categories and buy or sell your furniture .</p>
                         <button className="btn btn-primary">Get Started</button>
                     </div>
                 </div>
